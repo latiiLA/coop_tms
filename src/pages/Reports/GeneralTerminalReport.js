af5a65be69ca7dda317e186e9db191bdcff855fe
Lata <amelatbay@gmail.com>
2024-06-23 19:28:25 +0300

    general terminal report is added

---
+++ src/pages/Reports/GeneralTerminalReport.js
@@ -0,0 +1,23 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+import TerminalTypeReports from "./TerminalTypeReports";
+import TerminalSiteReport from "./TerminalSiteReport";
+
+const GeneralTerminalReport = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 2,
+        padding: "inherit",
+      }}
+    >
+      <Typography variant="h4">GeneralTerminalReport</Typography>
+      <TerminalTypeReports />
+      <TerminalSiteReport />
+    </Box>
+  );
+};
+
+export default GeneralTerminalReport;
