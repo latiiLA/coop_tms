d6297ea6783859660bb7dec1019866cd0844c8f9
Lata <amelatbay@gmail.com>
2024-06-23 19:26:38 +0300

    Manage atm is added

---
+++ src/pages/administration/ManageAtm.js
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+import React from "react";
+import View from "../../components/View";
+
+const ManageAtm = ({ role }) => {
+  return (
+    <Box>
+      <View role="admin" />
+    </Box>
+  );
+};
+
+export default ManageAtm;
