78b9592f55ce4b97ec87485b49e5920283575031
Lata <amelatbay@gmail.com>
2024-06-23 19:21:06 +0300

    footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <Box>
+      <Typography>
+        {" "}
+        &copy; 2024 Cooperative Bank of Oromia. All Rights Reserved | Designed,
+        Built and Maintained by Lata Amenu{" "}
+      </Typography>
+    </Box>
+  );
+};
+
+export default Footer;
