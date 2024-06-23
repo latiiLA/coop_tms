b99ded891dbc421177a3a96fd787b8aba1e06589
Lata <amelatbay@gmail.com>
2024-06-23 19:25:38 +0300

    Rightside home components added

---
+++ src/pages/rightside/RightsideHome.js
@@ -0,0 +1,12 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+const RightsideHome = () => {
+  return (
+    <Box backgroundColor="gray">
+      <Typography variant="h4"> Welcome</Typography>
+    </Box>
+  );
+};
+
+export default RightsideHome;
