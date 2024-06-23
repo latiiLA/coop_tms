f53fca28d5d0e5f434078c1d72162da40afdda8d
Lata <amelatbay@gmail.com>
2024-06-23 19:20:18 +0300

    Dark mode component is added

---
+++ src/DarkMode/DarkMode.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { ReactComponent as Sun } from "./Sun.svg";
+import { ReactComponent as Moon } from "./Moon.svg";
+import "./DarkMode.css";
+import { useTheme } from "../context/ThemeProvider";
+
+const DarkMode = () => {
+  const { darkMode, toggleTheme } = useTheme();
+  console.log(darkMode);
+  return (
+    <div className="dark_mode">
+      <input
+        className="dark_mode_input"
+        type="checkbox"
+        id="darkmode-toggle"
+        onClick={toggleTheme}
+      />
+      <label className="dark_mode_label" htmlFor="darkmode-toggle">
+        <Sun />
+        <Moon />
+      </label>
+    </div>
+  );
+};
+
+export default DarkMode;
