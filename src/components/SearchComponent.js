598ae503c97b33539951aeb028d4c4013cacaad9
Lata <amelatbay@gmail.com>
2024-07-29 13:33:04 +0300

    search component is modified

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,6 +1,7 @@
+import React, { useState } from "react";
+import axios from "axios";
 import { Box, Typography } from "@mui/material";
 import { styled, alpha } from "@mui/material/styles";
-import React from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import InputBase from "@mui/material/InputBase";
 
@@ -41,30 +42,65 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   width: "100%",
   "& .MuiInputBase-input": {
     padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
     transition: theme.transitions.create("width"),
     [theme.breakpoints.up("sm")]: {
-      width: "12ch",
+      width: "25ch",
       "&:focus": {
-        width: "20ch",
+        width: "30ch",
       },
     },
   },
 }));
 
 const SearchComponent = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8000/search?query=${searchQuery}`
+      );
+      setSearchResults(response.data.terminals); // Assuming 'terminals' is the key in your response object
+    } catch (error) {
+      console.error("Error searching:", error);
+      // Handle error
+    }
+  };
+
+  const handleChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevent default form submission behavior
+    handleSearch(); // Call handleSearch when form is submitted
+  };
+
   return (
     <Box>
-      <Search>
+      <Search component="form" onSubmit={handleSubmit}>
         <SearchIconWrapper>
           <SearchIcon />
         </SearchIconWrapper>
         <StyledInputBase
           placeholder="Search…"
           inputProps={{ "aria-label": "search" }}
+          value={searchQuery}
+          onChange={handleChange}
         />
       </Search>
+      {searchResults.length > 0 && (
+        <Box mt={2}>
+          <Typography variant="h5">Search Results:</Typography>
+          {searchResults.map((result) => (
+            <Typography key={result._id}>
+              {result.id} - {result.type}
+            </Typography>
+          ))}
+        </Box>
+      )}
     </Box>
   );
 };
