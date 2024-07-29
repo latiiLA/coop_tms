c86cb59c781be1135c18096994fc03377299dcc1
Lata <amelatbay@gmail.com>
2024-07-29 13:32:07 +0300

    Loading component is added

---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Box, CircularProgress } from "@mui/material";
+
+const LoadingSpinner = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <CircularProgress />
+    </Box>
+  );
+};
+
+export default LoadingSpinner;
