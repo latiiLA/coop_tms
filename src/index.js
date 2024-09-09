269427184f3edd48bd737b7f4106fd7e92c1d736
Lata <amelatbay@gmail.com>
2024-09-09 17:30:07 +0300

    Copy branch code functionality corrected

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -10,7 +9,6 @@ import { AuthContextProvider } from "./context/AuthContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  // <React.StrictMode>
   <Router>
     <AuthContextProvider>
       <ThemeProvider>
