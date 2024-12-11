fd1bb9cdf9e85718de4bd272396a57b5abd0605a
Lata <amelatbay@gmail.com>
2024-12-11 16:57:52 +0300

    updated

--- src/pages/administration/AddTerminal.js
+++ src/pages/administration/AddTerminal.js
@@ -1,15 +1,18 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Box } from "@mui/material";
 import axios from "axios";
 import toast from "react-hot-toast";
 import ATMForm from "../../components/ATMForm";
 import { useNavigate } from "react-router-dom";
+import LoadingSpinner from "../../components/LoadingSpinner";
 
 const AddTerminal = () => {
   const navigate = useNavigate();
   const hasShownToast = useRef(false); // Use ref to track if the toast has been shown
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (values, { setSubmitting }) => {
+    setLoading(true);
     const apiUrl = process.env.REACT_APP_API_URL;
     const token = localStorage.getItem("token");
 
@@ -33,7 +36,8 @@ const AddTerminal = () => {
           withCredentials: true,
         }
       );
-
+      // console.log("terminals,", response.data.terminal);
+      navigate("/viewdetail", { state: { row: response.data.terminal } });
       // console.log("New ATM is added:", response.data);
       toast.success(response.data.message);
     } catch (error) {
@@ -48,6 +52,7 @@ const AddTerminal = () => {
       hasShownToast.current = true;
     } finally {
       setSubmitting(false);
+      setLoading(false);
     }
   };
 
@@ -58,9 +63,29 @@ const AddTerminal = () => {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        margin: 1,
       }}
     >
       <ATMForm initialValues={{}} onSubmit={handleSubmit} isEdit={false} />
+      {/* Loading Spinner */}
+      {loading && (
+        <Box
+          sx={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            // marginLeft: 10,
+            zIndex: 10, // Ensures the spinner is above the form
+          }}
+        >
+          <LoadingSpinner />
+        </Box>
+      )}
     </Box>
   );
 };
