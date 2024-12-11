7da3bdebd7a7293b067d1af09a31f38357ae6e18
Lata <amelatbay@gmail.com>
2024-12-11 16:58:21 +0300

    updated

--- src/components/ViewTerminalGridComponent.js
+++ src/components/ViewTerminalGridComponent.js
@@ -1,28 +1,82 @@
-import { ContentCopy, Edit, Preview } from "@mui/icons-material";
-import { Box, Divider, IconButton, Tooltip, Typography } from "@mui/material";
+import {
+  Cancel,
+  CheckCircle,
+  ContentCopy,
+  Download,
+  Edit,
+  Preview,
+} from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Divider,
+  IconButton,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "../context/AuthContext";
 import toast from "react-hot-toast";
 import CustomToolbar from "./CustomToolbar";
+import { GrConfigure } from "react-icons/gr";
+import axios from "axios";
 
 const ViewTerminalGridComponent = ({ rows, isRelocated }) => {
   const navigate = useNavigate();
   const { role } = useAuthContext();
+  // console.log(pings);
+
+  const [open, setOpen] = useState(false);
+  const [configData, setConfigData] = useState("");
+
   const columns = [
     // { field: "id", headerName: "No", type: "number", width: 10 },
-    { field: "unitId", headerName: "Unit ID", type: "number", flex: 0.1 },
+    { field: "unitId", headerName: "Unit ID", type: "number", flex: 0.4 },
     { field: "terminalId", headerName: "Terminal ID", flex: 0.6 },
     { field: "terminalName", headerName: "Terminal Name", flex: 1 },
     { field: "branchName", headerName: "Branch Name", flex: 1 },
-    { field: "district", headerName: "District", flex: 0.8 },
+    { field: "district", headerName: "District", flex: 0.7 },
     { field: "cbsAccount", headerName: "CBS Account", flex: 0.8 },
     { field: "port", headerName: "Port", flex: 0.1 },
-    { field: "ipAddress", headerName: "IP Address", flex: 1 },
+    { field: "ipAddress", headerName: "IP Address", flex: 0.8 },
     { field: "type", headerName: "Type", flex: 0.1 },
-    { field: "site", headerName: "Site", flex: 0.5 },
-    { field: "status", headerName: "Status", flex: 0.5 },
+    { field: "site", headerName: "Site", flex: 0.4 },
+    // // ip_terminal_id
+    // {!isRelocated && (
+    //   {
+    //     field: "EJ",
+    //     headerName: "EJ",
+    //     flex: 0.1,
+    //     renderCell: (params) => (
+    //       <Box
+    //         sx={{
+    //           display: "flex",
+    //           flexDirection: "row",
+    //           justifyContent: "space-around",
+    //           width: "100%",
+    //           height: "100%",
+    //           margin: "auto",
+    //           alignItems: "center",
+    //         }}
+    //       >
+    //         <Tooltip title="Download EJ File">
+    //           <IconButton
+    //             color="primary"
+    //             size="small"
+    //             onClick={() => handleEJFile(params.row)}
+    //           >
+    //             <Download />
+    //           </IconButton>
+    //         </Tooltip>
+    //       </Box>
+    //     ),
+    //   })},
     {
       field: "actions",
       headerName: "Actions",
@@ -74,6 +128,19 @@ const ViewTerminalGridComponent = ({ rows, isRelocated }) => {
               <ContentCopy />
             </IconButton>
           </Tooltip>
+          {!isRelocated && (
+            <Tooltip title="Generate Config">
+              <IconButton
+                color="primary"
+                size="small"
+                disabled={role === "user"}
+                style={{ display: role === "user" ? "none" : "inline-flex" }}
+                onClick={() => handleConfig(params.row)}
+              >
+                <GrConfigure />
+              </IconButton>
+            </Tooltip>
+          )}
         </Box>
       ),
     },
@@ -112,6 +179,57 @@ const ViewTerminalGridComponent = ({ rows, isRelocated }) => {
     }
   };
 
+  const handlePortName = async (portNumber) => {
+    console.log(portNumber);
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      toast.error("User is not authenticated");
+      navigate("/home");
+      return; // Exit the function if no token is found
+    }
+
+    try {
+      const apiUrl = process.env.REACT_APP_API_URL;
+      const response = await axios.get(
+        `${apiUrl}/port/getPortName`, // Use the correct endpoint
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          params: { portNumber }, // Pass portNumber as a query parameter
+          withCredentials: true,
+        }
+      );
+      // console.log("return inside handleportName", response.data.portName);
+      return response.data.portName; // Return the fetched port name
+    } catch (error) {
+      if (error.response?.data?.message) {
+        toast.error(error.response.data.message);
+      } else {
+        toast.error(error.message);
+      }
+
+      return null; // Signal failure with a return value
+    }
+  };
+
+  const handleConfig = async (rowData) => {
+    let type = "CBOBNA";
+    if (rowData.type === "NCR") {
+      type = "CBONCR";
+    }
+    // console.log(rowData);
+
+    const portName = await handlePortName(rowData.port);
+    console.log("inside handle config", portName);
+    const portNameLowercase = String(portName).toLowerCase();
+
+    const configData = `port.name  atm.1.${type}.${rowData.unitId}  CurrentNode  DnAtmServer  external  listen  <cmmt:CMMT_${portName}.*.${rowData.port}|${rowData.ipAddress}.*::cmmt_${portNameLowercase}>`;
+    setConfigData(configData); // Set config data
+    setOpen(true); // Open dialo
+  };
+
   // Fallback method using a temporary textarea for older browsers or HTTP
   const fallbackCopyText = (text) => {
     const textArea = document.createElement("textarea");
@@ -131,6 +249,41 @@ const ViewTerminalGridComponent = ({ rows, isRelocated }) => {
 
     // Remove the textarea element after copying
     document.body.removeChild(textArea);
+  };
+  const configCopy = (text) => {
+    if (!text) {
+      toast.error("No config details to copy!");
+      return;
+    }
+
+    // Ensure the text is a string
+    const textToCopy =
+      typeof text === "object" ? JSON.stringify(text, null, 2) : text;
+    console.log("textToCopy", textToCopy);
+
+    try {
+      if (navigator.clipboard) {
+        navigator.clipboard.writeText(textToCopy); // Modern API
+        toast.success("Config details copied to clipboard!");
+      } else {
+        const textArea = document.createElement("textarea");
+        textArea.value = textToCopy;
+        textArea.style.position = "fixed"; // Prevent scrolling
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+        document.execCommand("copy");
+        document.body.removeChild(textArea);
+        toast.success("Config details copied to clipboard!");
+      }
+    } catch (err) {
+      // console.error("Copy failed:", err);
+      toast.error("Failed to copy config details.");
+    }
+  };
+
+  const handleClose = () => {
+    setOpen(false);
   };
 
   return (
@@ -163,6 +316,7 @@ const ViewTerminalGridComponent = ({ rows, isRelocated }) => {
           "& .MuiDataGrid-virtualScroller::-webkit-scrollbar-thumb": {
             display: "none",
           },
+          marginX: 1,
         }}
       >
         <DataGrid
@@ -182,6 +336,24 @@ const ViewTerminalGridComponent = ({ rows, isRelocated }) => {
           autoHeight
           checkboxSelection
         />
+        {/* Dialog for Config Data */}
+        <Dialog open={open} onClose={handleClose} maxWidth="sm" fullWidth>
+          <DialogTitle>Configuration Data</DialogTitle>
+          <DialogContent>
+            <Typography variant="body1">{configData}</Typography>
+          </DialogContent>
+          <DialogActions>
+            <Button
+              onClick={() => {
+                configCopy(configData);
+                handleClose();
+              }}
+              color="primary"
+            >
+              Copy
+            </Button>
+          </DialogActions>
+        </Dialog>
       </Box>
     </Box>
   );
