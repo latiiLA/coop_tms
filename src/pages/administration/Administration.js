c027339d28a0c1907774c54feb07152ae061d872
Lata <amelatbay@gmail.com>
2024-12-11 16:57:15 +0300

    view all adminstrationcomponents into one

--- src/pages/administration/Administration.js
+++ src/pages/administration/Administration.js
@@ -1,8 +1,13 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Divider, Typography } from "@mui/material";
 import React from "react";
 
 import CreateUser from "./CreateUser";
 import ViewUsers from "./ViewUsers";
+import ViewFeedback from "../../components/ViewFeedback";
+import ViewBugs from "../../components/ViewBugs";
+
+import UserActivityLog from "./UserActivityLog";
+import Analytics from "../dashboard/Analytics";
 
 const Administration = () => {
   return (
@@ -11,24 +16,38 @@ const Administration = () => {
         display: "flex",
         flexDirection: "column",
         gap: 5,
-        padding: "inherit",
+        padding: 2,
       }}
     >
       <Box>
+        <Typography variant="h4">Create User</Typography>
+        <CreateUser />
+      </Box>
+      <Divider />
+      <Box>
         <Typography variant="h4">Manage Users</Typography>
-        <CreateUser />
-      </Box>
-      <Box>
-        <Typography variant="h4">ATM</Typography>
-        <CreateUser />
-      </Box>
-      <Box>
-        <Typography variant="h4">Users</Typography>
         <ViewUsers />
       </Box>
+      <Divider />
+
       <Box>
-        <Typography variant="h4">ATM</Typography>
-        <ViewUsers />
+        <Typography variant="h4">Analytics</Typography>
+        <Analytics />
+      </Box>
+      <Divider />
+      <Box>
+        <Typography variant="h4">Activity Log</Typography>
+        <UserActivityLog />
+      </Box>
+      <Divider />
+      <Box>
+        <Typography variant="h4">Explore Feedback</Typography>
+        <ViewFeedback />
+      </Box>
+      <Divider />
+      <Box>
+        <Typography variant="h4">Explore Bug</Typography>
+        <ViewBugs />
       </Box>
     </Box>
   );
