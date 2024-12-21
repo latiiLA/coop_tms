08ccec05dfb1fdba9d19124499264233264f36e3
Lata <amelatbay@gmail.com>
2024-12-21 11:09:17 +0300

    global theme created

---
+++ src/DarkMode/demoTheme.js
@@ -0,0 +1,20 @@
+import { createTheme } from "@mui/material";
+
+const demoTheme = createTheme({
+  cssVariables: {
+    colorSchemeSelector: "data-toolpad-color-scheme",
+  },
+
+  colorSchemes: { light: true, dark: true },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 600,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+});
+
+export default demoTheme;
