597b18aa31781ac08d7f2e0c78c5994d8a9f1150
Lata <amelatbay@gmail.com>
2024-12-24 10:50:55 +0300

    bulk request functionality added

--- src/pages/POS/requests/BulkRequest.js
+++ src/pages/POS/requests/BulkRequest.js
@@ -1,55 +1,139 @@
-import React, { useState } from "react";
-import { Box, Button } from "@mui/material";
+import React, { useRef, useState } from "react";
+import { Box, Button, Typography } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import * as XLSX from "xlsx";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { SimCard } from "@mui/icons-material";
+import toast from "react-hot-toast";
+import * as yup from "yup";
+import LoadingButton from "@mui/lab/LoadingButton";
+import { CloudUpload } from "@mui/icons-material";
 
 const BulkRequest = () => {
   const [columns, setColumns] = useState([]);
   const [rows, setRows] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const fileInputRef = useRef(null);
   const navigate = useNavigate();
+  const phoneRegExp = /^[0-9]{9}$/;
 
-  const handleFileUpload = (e) => {
+  // Define the Yup validation schema for each row
+  const rowSchema = yup.object().shape({
+    column_1: yup.string().required("POS District is required"),
+    column_2: yup.string().required("POS Branch Name is required"),
+    column_3: yup.string().required("Merchant Name is required"),
+    column_4: yup
+      .string()
+      .required("POS Type/Site(Merchant or Branch) is required")
+      .test(
+        "is-merchant-or-branch",
+        "POS Type Must be either 'Merchant' or 'Branch' (case-insensitive)",
+        (value) =>
+          value && ["merchant", "branch"].includes(value.trim().toLowerCase())
+      ),
+    column_5: yup
+      .string()
+      .required("POS CBS Account is required")
+      .min(13, "The CBS account should be exactly 13 characters.")
+      .max(13, "The CBS account should be exactly 13 characters."),
+    column_6: yup.string().required("Contact Name is required"),
+    column_7: yup
+      .string()
+      .required("Merchant Phone Number is required")
+      .matches(phoneRegExp, "Phone number must be 9 digits"),
+    column_8: yup.string().required("Merchant Address is required"),
+    column_9: yup
+      .string()
+      .required("Serial number is required.")
+      .min(16, "Serial number must at least 16 character.")
+      .max(16, "Serial number must at least 16 character."),
+    column_10: yup
+      .string()
+      .required("Service number is required")
+      .matches(phoneRegExp, "Phone number must be 10 digits"),
+    column_11: yup
+      .string()
+      .required("IP Address is required")
+      .matches(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/, "Invalid IP Address format")
+      .test("is-valid-ip", "Invalid IP Address", (value) => {
+        const parts = value.split(".");
+        if (parts.length !== 4) return false;
+        return parts.every((part) => {
+          const num = parseInt(part, 10);
+          return !isNaN(num) && num >= 0 && num <= 255;
+        });
+      }),
+  });
+
+  const handleFileChange = (event) => {
+    console.log("File selected: ", event.target.files[0]); // Log the selected file
+    setSelectedFile(event.target.files[0]);
+    handleFileUpload(event);
+    if (fileInputRef.current) {
+      fileInputRef.current.value = "";
+    }
+  };
+
+  const handleFileUpload = async (e) => {
+    console.log("Handle upload is called");
     const file = e.target.files[0];
-    if (file) {
+    if (file && (file.name.endsWith(".xlsx") || file.name.endsWith(".xls"))) {
       const reader = new FileReader();
-      reader.onload = (event) => {
+      reader.onload = async (event) => {
         const binaryStr = event.target.result;
         const workbook = XLSX.read(binaryStr, { type: "binary" });
         const sheetName = workbook.SheetNames[0];
         const sheet = workbook.Sheets[sheetName];
         const sheetData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
 
+        console.log("Sheet data: ", sheetData); // Log the sheet data
+
         if (sheetData.length > 0) {
-          // Create columns from the first row
           const newColumns = sheetData[0].map((header, index) => ({
-            field: `column_${index}`, // Correct template literal
-            headerName: header || `Column ${index + 1}`, // Correct template literal
+            field: `column_${index}`,
+            headerName: header || `Column ${index + 1}`,
             flex: 1,
           }));
 
-          // Create rows from the remaining data
-          const newRows = sheetData.slice(1).map((row, rowIndex) => {
+          const newRows = [];
+          for (let rowIndex = 1; rowIndex < sheetData.length; rowIndex++) {
+            const row = sheetData[rowIndex];
             const rowData = {};
             row.forEach((cell, cellIndex) => {
-              rowData[`column_${cellIndex}`] = cell; // Correct template literal
+              rowData[`column_${cellIndex}`] = cell;
             });
-            return { id: rowIndex + 1, ...rowData };
-          });
 
-          // Assuming setColumns and setRows are state setters (if using React)
-          setColumns(newColumns);
+            try {
+              await rowSchema.validate(rowData, { abortEarly: false });
+              newRows.push({
+                id: rowIndex + 1,
+                ...rowData,
+                uploadedFile: null,
+              });
+            } catch (err) {
+              setRows([]);
+              setColumns([]);
+              toast.error(
+                `Row ${rowIndex + 1} validation errors: ${err.errors.join(", ")}`
+              );
+              return;
+            }
+          }
+
+          // Update state with valid rows and columns
           setRows(newRows);
+          setColumns(newColumns);
         }
       };
       reader.readAsBinaryString(file);
+    } else {
+      toast.error("Please upload a valid Excel file.");
     }
   };
 
   const handleSubmit = async () => {
+    console.log("Submit called, sending request..."); // Log the submit trigger
     setLoading(true);
     const apiUrl = process.env.REACT_APP_API_URL;
     const token = localStorage.getItem("token");
@@ -58,45 +142,72 @@ const BulkRequest = () => {
       navigate("/home");
       return;
     }
+
     try {
-      // Replace the URL with your server endpoint
-      console.log("inside handle bulk submit", rows);
+      console.log("Sending rows to backend:", rows); // Log the data being sent
       const response = await axios.post(
         `${apiUrl}/request/bulkrequest`,
         { rows },
         {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
+          headers: { Authorization: `Bearer ${token}` },
           withCredentials: true,
         }
       );
-      console.log("Bulk request submitted successfully:", response.data);
-      alert("Bulk request submitted successfully!");
+      toast.success("The request has been successfully updated.");
     } catch (error) {
-      console.error("Error submitting bulk request:", error);
-      alert("Error submitting bulk request. Please try again.");
+      toast.error(
+        error.response?.data?.message || error.message || "Something went wrong"
+      );
     } finally {
       setLoading(false);
     }
   };
 
+  const handleCancelRequest = async () => {
+    setRows([]);
+    setColumns([]);
+    setSelectedFile(null);
+    fileInputRef.current.value = null;
+  };
+
   return (
-    <Box sx={{ margin: 2 }}>
+    <Box sx={{ margin: 2, gap: 2 }}>
       <Box sx={{ marginBottom: 2, display: "flex", gap: 2 }}>
-        <Button variant="contained" component="label">
-          Upload Excel File
+        <Typography variant="h5">Bulk Request</Typography>
+        <Button
+          variant="outlined"
+          component="label"
+          startIcon={<CloudUpload />}
+        >
+          {selectedFile ? "Change File" : "Upload File"}
           <input
             type="file"
             hidden
+            ref={fileInputRef}
+            onChange={handleFileChange}
             accept=".xlsx, .xls"
-            onChange={handleFileUpload}
+            key={selectedFile ? selectedFile.name : "new"}
           />
         </Button>
+        {selectedFile && (
+          <Typography variant="body2" sx={{ marginY: "auto" }}>
+            {selectedFile.name}
+          </Typography>
+        )}
         {rows.length > 0 && (
-          <Button variant="contained" color="primary" onClick={handleSubmit}>
-            Submit Bulk Request
-          </Button>
+          <>
+            <LoadingButton
+              loading={loading}
+              variant="contained"
+              color="primary"
+              onClick={handleSubmit}
+            >
+              Submit Bulk Request
+            </LoadingButton>
+            <Button variant="contained" onClick={handleCancelRequest}>
+              Cancel Request
+            </Button>
+          </>
         )}
       </Box>
       <Box sx={{ width: "100%" }}>
