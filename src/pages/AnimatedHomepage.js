3f608dfc8105cfab38be11d24216a340e14d887c
Lata <amelatbay@gmail.com>
2024-12-24 10:51:37 +0300

    animated home page

--- src/pages/AnimatedHomepage.js
+++ src/pages/AnimatedHomepage.js
@@ -1,14 +1,17 @@
 import React from "react";
 import Lottie from "lottie-react";
-import TMSAnimation from "./tms-animation.json"; // Replace with your Lottie JSON file
+import TMSAnimation from "../assets/animatewelcome.lottie"; // Replace with your Lottie JSON file
+import { useTheme } from "@mui/material";
 
 const AnimatedHomePage = () => {
+  const theme = useTheme();
   return (
     <div style={{ textAlign: "center", marginTop: "20px" }}>
       <h1>Welcome to Terminal Management System</h1>
       <Lottie
         animationData={TMSAnimation}
         style={{ width: 300, height: 300 }}
+        sx={{ backgroundColor: theme.palette.background.default }}
       />
       <p>Connecting Terminals with Banks Seamlessly</p>
     </div>
