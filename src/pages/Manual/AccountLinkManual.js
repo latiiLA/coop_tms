46e479fa15188254b172b5492fe7a42abd28cc8b
Lata <amelatbay@gmail.com>
2025-03-18 10:46:48 +0300

    view account to terminal link guide pdf

--- src/pages/Manual/AccountLinkManual.js
+++ src/pages/Manual/AccountLinkManual.js
@@ -2,7 +2,31 @@ import { Box } from "@mui/material";
 import React from "react";
 
 const AccountLinkManual = () => {
-  return <Box>Account Link Manual</Box>;
+  // Replace with the modified Google Drive preview URL
+  const pdfUrl = `https://docs.google.com/document/d/1WsOkMMuY4UHkzQHxIpMXP_sLcI0jAAGeRdSjSpxEBQk/preview`;
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        marginY: 1,
+        marginX: "auto",
+        height: "100%",
+        width: "70%",
+        border: 1,
+      }}
+    >
+      <iframe
+        title="Terminal Creation Manual"
+        src={pdfUrl}
+        frameBorder="0"
+        height="100%"
+        width="100%"
+      ></iframe>
+    </Box>
+  );
 };
 
 export default AccountLinkManual;
